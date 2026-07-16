78856053e4116e734f6c26e0a68f7abb248a04e1
Lemar Cimanga <jeanlemar132@gmail.com>
2026-07-16 19:27:36 +0100

    Ajout de generateMatricule() dans User

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,13 @@ const pool = require('../config/database');
 const bcrypt = require('bcryptjs');
 
 class User {
+  static async generateMatricule() {
+    const prefix = 'KASH';
+    const timestamp = Date.now().toString().slice(-6);
+    const random = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
+    return `${prefix}${timestamp}${random}`;
+  }
+
   static async create(userData) {
     const { 
       nom, postnom, prenom, matricule, numero_telephone, 
